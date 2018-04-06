e1b9b90a1de633c93dc649503f2412cd286f6103
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-06 15:24:47 +0100

    Add perms.

--- src/ClearChat/ClearChat/Main.php
+++ src/ClearChat/ClearChat/Main.php
@@ -27,6 +27,7 @@ class Main extends PluginBase implements Listener{
 	
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		if(strtolower($command->getName()) == "cc"){
+		    if($sender->hasPermission("clearchat.clear")) {
 			$senderName = $sender->getName();
 			$this->getServer()->broadcastMessage("§aChat Being cleared");
 			$this->getServer()->broadcastMessage("§athis can lead to many things");
