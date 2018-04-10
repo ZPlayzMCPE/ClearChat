be22d0208eba269c7d89007988968fa342c7c825
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-10 07:56:14 +0100

    Removed Unused imports
    Clean up imports.

--- src/ClearChat/ClearChat/Main.php
+++ src/ClearChat/ClearChat/Main.php
@@ -2,20 +2,9 @@
 namespace ClearChat\ClearChat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\block\SignChangeEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\level\particle\FlameParticle;
-use pocketmine\level\sound\LaunchSound;
-use pocketmine\math\Vector3;
-use pocketmine\utils\TextFormat as Color;
-use pocketmine\item\Item;
-use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\{Server, Player};
+
 class Main extends PluginBase implements Listener{
 	
 		public function onEnable(){
